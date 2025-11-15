932ae85952834f4803fb1fceee5439c145a95245
DiStudd <vasiliyushkov.8511@gmail.com>
2025-11-16 02:38:17 +0300

    test add init struct and libs

--- src/lynkapp.js
+++ src/lynkapp.js
@@ -1,12 +1,14 @@
 import express, {json} from "express"
+import cors from "cors"
 const app = express()
 
 const PORT = 3000
 
-app.use(json)
+app.use(json())
+app.use(cors())
 
 app.get("/", (req, res) => {
-    res.send('lynk started').status(201)
+    res.status(201).send({app_status: 'app is working ok'})
 })
 
 app.listen(PORT, () => {
